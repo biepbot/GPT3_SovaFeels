d3cf4e0684838693644bbedbc9cfd37b13a97c10
Zeronium <riccardo.scoppetta@gmail.com>
2016-12-08 11:29:28 +0100

    Good commit

---
+++ CustomPlatformerController/Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour
+{
+	public Camera playerCamera;
+	public GameObject interactableIcon;
+	public Text saidField;
+	public Button[] awnserButtons = new Button[4];
+
+	public GameObject dialogPannel;
+
+	private bool isActive = false;
+	private Transform interactableTransform;
+
+	private void Awake()
+	{
+		interactableTransform = interactableIcon.transform;
+	}
+
+	private void LateUpdate()
+	{
+		interactableIcon.SetActive(isActive);
+		isActive = false;
+	}
+
+	public void SetInteractable(Vector3 worldPos)
+	{
+		isActive = true;
+		Vector3 NewPos = playerCamera.WorldToScreenPoint(worldPos);
+		interactableTransform.position = NewPos;
+	}
+
+	public void SetDialog(string[] textInfo)
+	{
+		saidField.text = textInfo[0];
+		if (textInfo.Length > 1)
+		{
+			awnserButtons[0].transform.GetChild(0).GetComponent<Text>().text = textInfo[1];
+			awnserButtons[1].transform.GetChild(0).GetComponent<Text>().text = textInfo[2];
+			awnserButtons[2].transform.GetChild(0).GetComponent<Text>().text = textInfo[3];
+			awnserButtons[3].transform.GetChild(0).GetComponent<Text>().text = textInfo[4];
+		}
+		else
+		{
+		}
+
+		dialogPannel.SetActive(true);
+
+	}
+
+	public void RemoveDialog()
+	{
+		dialogPannel.SetActive(false);
+	}
+}
---
+++ CustomPlatformerController/Assets/Scripts/PlayerSprint.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerSprint : MonoBehaviour
+{
+	public float moveSpeed = 2f;
+	public float jumpingHeight = 0.2f;
+	public float gravity = -0.5f;
+	public float smoothTime = 0.1f;
+	public float dashSpeed = 10f;
+	public float sprintTime = 0f;
+	public float maxSprintTime = 2f;
+	public float staminaRegenSpeed = 0.25f;
+
+	private Vector3 velocity;
+	private float velocityTarget;
+	private float smoothVelocity;
+	private bool sprinting = false;
+
+	private PlayerController playerController;
+
+	void Awake()
+	{
+		playerController = GetComponent<PlayerController>();
+	}
+
+	void Update()
+	{
+		if (playerController.collInfo.below || playerController.collInfo.above) velocity.y = 0;
+		int inputX = 0;
+
+		if (Input.GetKey(KeyCode.LeftArrow)) inputX -= 1;
+		if (Input.GetKey(KeyCode.RightArrow)) inputX += 1;
+		if (Input.GetKey(KeyCode.UpArrow) && playerController.collInfo.below) velocity.y = jumpingHeight;
+
+
+
+		if (Input.GetKey(KeyCode.DownArrow) && sprintTime > 0 && (sprintTime > maxSprintTime / 3 || sprinting) && inputX != 0)
+		{
+			sprinting = true;
+			sprintTime -= Time.deltaTime;
+			velocityTarget = inputX * dashSpeed * Time.deltaTime;
+		}
+		else
+		{
+			sprinting = false;
+			if (sprintTime < maxSprintTime) sprintTime += staminaRegenSpeed * Time.deltaTime;
+			velocityTarget = inputX * moveSpeed * Time.deltaTime;
+		}
+
+		velocity.x = Mathf.SmoothDamp(velocity.x, velocityTarget, ref smoothVelocity, smoothTime);
+
+		if (!playerController.collInfo.below) velocity.y += gravity * Time.deltaTime;
+
+		playerController.Move(velocity);
+	}
+}
