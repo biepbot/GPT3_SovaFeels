0a9dca2bcc99afd42ec3ff42181f5db98256ee86
jesse deij <jessedeij@hotmail.com>
2016-12-09 13:34:16 +0100

    basis menus
    2 scenes toegevoegd mainmenuscene is voor t main menu en de
    pauzetestscene die ik alleen heb voor het testen van het pauzescherm ook
    heb ik nu eff alle buttons in mainmenu gelinkt naar die scene zodat als
    je een playthrough start/continued je naar die scene gaat

---
+++ CustomPlatformerController/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Button startnew;
+    public Button continueold;
+
+    public void StartNewPlaythrough()
+    {
+        SceneManager.LoadScene("Pauzetestscene");
+        // ToDo: generate a new playthrough and load the first level of that playthrough, right now its just set to load up my testscene
+    }
+
+    public void ContinueOldPlaythrough()
+    {
+        SceneManager.LoadScene("Pauzetestscene");// just set to load my testscene just like the above method
+        // ToDo: make it possible to select an old playthrough? or is there only one playthrough at a time if so then just load up that one
+    }
+}
---
+++ CustomPlatformerController/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public Button pausebutton;
+    public Button resumebutton;
+    public Button mainmenubutton;
+    public GameObject pausepanel;
+
+    public void PauseGame()
+    {
+        pausepanel.SetActive(true);
+        pausebutton.gameObject.SetActive(false);
+
+        // ToDo: disable player controls and maybe also pause the game(stop all moving things and stuff) instead of just showing a menu overlay
+    }
+
+    public void ResumeGame()
+    {
+        pausepanel.SetActive(false);
+        pausebutton.gameObject.SetActive(true);
+    }
+
+    public void ReturnToMain()
+    {
+        // ToDo: save all character stuff that hasnt been saved
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
