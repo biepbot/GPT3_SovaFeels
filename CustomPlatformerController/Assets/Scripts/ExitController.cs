e74d26283a5b89cab3a89abe663cbde7e55b46e2
Tim Goes <Tim_Goes4@hotmail.nl>
2016-12-16 14:20:45 +0100

    Playthrough kan je nu afronden

--- CustomPlatformerController/Assets/Scripts/ExitController.cs
+++ CustomPlatformerController/Assets/Scripts/ExitController.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ExitController : MonoBehaviour {
+
+    public GameObject endGamePrefab;
+
+    private void Awake()
+    {
+        if(endGamePrefab == null)
+        {
+            endGamePrefab = Instantiate(Resources.Load("EndMenu/EndGameMenu", typeof(GameObject))) as GameObject;
+
+            endGamePrefab.SetActive(false);
+        }
+    }
+
     public void NextLevel()
     {
         if (LevelLoader.HasMoreLevels)
@@ -17,11 +30,12 @@ public class ExitController : MonoBehaviour {
 		}
 		else
 		{
-			
+            endGamePrefab.SetActive(true);
 
+            /*
 			Debug.LogWarning("Game finished. All " + LevelLoader.DEFAULT_LEVEL_AMOUNT + " levels were finished\r\nUh. Do something now");
             //TODO
-            //Code for completing the game.
+            //Code for completing the game.*/
         }
     }
 }
