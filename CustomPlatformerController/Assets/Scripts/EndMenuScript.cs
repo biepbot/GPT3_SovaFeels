7eb0e4812e0626e8efdb7322900c607857102b25
Tim Goes <Tim_Goes4@hotmail.nl>
2016-12-16 13:51:10 +0100

    Fixed UI resolution fix statsscreen, endgamepanel

---
+++ CustomPlatformerController/Assets/Scripts/EndMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Base;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndMenuScript : MonoBehaviour {
+
+	public void LoadMainMenu()
+    {
+        LevelLoader.LoadMainMenu();
+    }
+
+    public void LoadStatistics()
+    {
+        LevelLoader.LoadStatistics();
+    }
+}
