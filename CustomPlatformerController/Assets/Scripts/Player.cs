52d2c6500c83d4590e9f39c45fa9d90f84f73c77
Zeronium <riccardo.scoppetta@gmail.com>
2017-01-12 15:05:08 +0100

    Level 15 + player directions

--- CustomPlatformerController/Assets/Scripts/Player.cs
+++ CustomPlatformerController/Assets/Scripts/Player.cs
@@ -5,128 +5,134 @@ using UnityStandardAssets.CrossPlatformInput;
 [RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpingHeight = 15f;
-    public float gravity = 30f;
-    public float smoothTime = 0.15f;
+	public float moveSpeed = 5f;
+	public float jumpingHeight = 15f;
+	public float gravity = 30f;
+	public float smoothTime = 0.15f;
 
-    public PlayerInfo playerInfo;
+	public PlayerInfo playerInfo;
 
-    private Vector3 velocity;
-    private float velocityTarget;
-    private float smoothVelocity;
+	private Vector3 velocity;
+	private float velocityTarget;
+	private float smoothVelocity;
 
-    private PlayerController playerController;
-    private SoundManager soundManager;
+	private PlayerController playerController;
+	private SoundManager soundManager;
 
-    void Awake()
-    {
-        playerController = GetComponent<PlayerController>();
+	public Transform spriteTransform;
 
-        if (Camera.main.GetComponent<CameraController>() == null)
-        {
-            CameraController c = Camera.main.gameObject.AddComponent<CameraController>();
-            c.AddPlayer(this.gameObject);
-        }
+	void Awake()
+	{
+		playerInfo.direction = -2;
+		playerController = GetComponent<PlayerController>();
 
-        if (MobileHelper.OnMobileDevice)
-        {
-            Instantiate(Resources.Load("MobileSingleStickControl"));
-        }
-    }
+		if (Camera.main.GetComponent<CameraController>() == null)
+		{
+			CameraController c = Camera.main.gameObject.AddComponent<CameraController>();
+			c.AddPlayer(this.gameObject);
+		}
 
-    void Start()
-    {
-        soundManager = GameObject.FindObjectOfType<SoundManager>();
-    }
+		if (MobileHelper.OnMobileDevice)
+		{
+			Instantiate(Resources.Load("MobileSingleStickControl"));
+		}
+	}
 
-    void Update()
-    {
-        CheckYBump();
+	void Start()
+	{
+		soundManager = GameObject.FindObjectOfType<SoundManager>();
+	}
 
-        CheckXMovement();
+	void Update()
+	{
+		CheckYBump();
 
-        CheckJump();
+		CheckXMovement();
 
-        //Enable gravity to the player, if the player is not standing on a platform
-        if (!playerController.collInfo.below) velocity.y -= gravity * Time.deltaTime;
+		CheckJump();
 
-        //Move the player with the adjusted speeds
-        playerController.Move(velocity);
+		//Enable gravity to the player, if the player is not standing on a platform
+		if (!playerController.collInfo.below) velocity.y -= gravity * Time.deltaTime;
 
-        if (Input.GetAxis("Interact") == 1)
-        {
-            playerController.Interact();
-        }
-    }
+		//Move the player with the adjusted speeds
+		playerController.Move(velocity);
 
-    /// <summary>
-    /// Checks if the player is holding any buttons to jump
-    /// </summary>
-    private void CheckJump()
-    {
-        //If the player is pressing the positive vertical axis input, or jump input, the character needs to jump
-        bool doJump = false;
+		if (Input.GetAxis("Interact") == 1)
+		{
+			playerController.Interact();
+		}
+	}
 
-        if (MobileHelper.OnMobileDevice)
-        {
-            doJump = CrossPlatformInputManager.GetButton("Jump") ||
-                CrossPlatformInputManager.GetButton("Jump") && (int)CrossPlatformInputManager.GetAxis("Horizontal") != 0 ||
-                (int)CrossPlatformInputManager.GetAxis("Horizontal") != 0 && CrossPlatformInputManager.GetButton("Jump");
-        }
-        else
-        {
-            doJump = Input.GetAxis("Vertical") == 1 || Input.GetAxis("Jump") == 1;
-        }
+	/// <summary>
+	/// Checks if the player is holding any buttons to jump
+	/// </summary>
+	private void CheckJump()
+	{
+		//If the player is pressing the positive vertical axis input, or jump input, the character needs to jump
+		bool doJump = false;
 
-        //If the player needs to jump, and is able to, jump.
-        if (doJump && playerController.collInfo.below)
-        {
-            velocity.y = jumpingHeight;
-            if (SoundManager.Instance != null)
-            {
-                SoundManager.Instance.ObjectSounds[0].PlayAudioClip(0);
-            }
-        }
-    }
+		if (MobileHelper.OnMobileDevice)
+		{
+			doJump = CrossPlatformInputManager.GetButton("Jump") ||
+				CrossPlatformInputManager.GetButton("Jump") && (int)CrossPlatformInputManager.GetAxis("Horizontal") != 0 ||
+				(int)CrossPlatformInputManager.GetAxis("Horizontal") != 0 && CrossPlatformInputManager.GetButton("Jump");
+		}
+		else
+		{
+			doJump = Input.GetAxis("Vertical") == 1 || Input.GetAxis("Jump") == 1;
+		}
 
-    /// <summary>
-    /// Checks if the player is holding any buttons to move left / right
-    /// </summary>
-    private void CheckXMovement()
-    {
-        int inputX;
-        if (MobileHelper.OnMobileDevice)
-        {
-            inputX = (int)CrossPlatformInputManager.GetAxis("Horizontal");
-        }
-        else
-        {
-            inputX = (int)Input.GetAxis("Horizontal");
-        }
+		//If the player needs to jump, and is able to, jump.
+		if (doJump && playerController.collInfo.below)
+		{
+			velocity.y = jumpingHeight;
+			if (SoundManager.Instance != null)
+			{
+				SoundManager.Instance.ObjectSounds[0].PlayAudioClip(0);
+			}
+		}
+	}
 
-        //The movement over the X axis is defined by the input of the Horizontal axis
-        playerInfo.direction = inputX;
-        velocityTarget = inputX * moveSpeed;
-        velocity.x = Mathf.SmoothDamp(velocity.x, velocityTarget, ref smoothVelocity, smoothTime);
-    }
+	/// <summary>
+	/// Checks if the player is holding any buttons to move left / right
+	/// </summary>
+	private void CheckXMovement()
+	{
+		int inputX;
+		if (MobileHelper.OnMobileDevice)
+		{
+			inputX = (int)CrossPlatformInputManager.GetAxis("Horizontal");
+		}
+		else
+		{
+			inputX = (int)Input.GetAxis("Horizontal");
+		}
 
-    /// <summary>
-    /// Checks if the player bumps into anything on the y-axis, and adjusts their speed if this is the case
-    /// </summary>
-    private void CheckYBump()
-    {
-        //If the player is colliding above or below, remove his speed
-        if (playerController.collInfo.below || playerController.collInfo.above) velocity.y = 0;
-    }
+		//The movement over the X axis is defined by the input of the Horizontal axis
+		if (inputX > 0.1f) playerInfo.direction = 1;
+		else if (inputX < -0.1f) playerInfo.direction = -1;
+		if (playerInfo.direction == 1) spriteTransform.eulerAngles = Vector3.zero;
+		else if (playerInfo.direction == -1) spriteTransform.eulerAngles = Vector3.up * 180;
+		velocityTarget = inputX * moveSpeed;
+		velocity.x = Mathf.SmoothDamp(velocity.x, velocityTarget, ref smoothVelocity, smoothTime);
+	}
 
-    public void PlatformMove(Vector3 velocity)
-    {
-        playerController.PlatformMove(velocity);
-    }
+	/// <summary>
+	/// Checks if the player bumps into anything on the y-axis, and adjusts their speed if this is the case
+	/// </summary>
+	private void CheckYBump()
+	{
+		//If the player is colliding above or below, remove his speed
+		if (playerController.collInfo.below || playerController.collInfo.above) velocity.y = 0;
+	}
 
-    public struct PlayerInfo
-    {
-        public int direction; // Left: -1; Right: 1;
-    }
+	public void PlatformMove(Vector3 velocity)
+	{
+		playerController.PlatformMove(velocity);
+	}
+
+	public struct PlayerInfo
+	{
+		public int direction; // Left: -1; Right: 1;
+	}
 }
