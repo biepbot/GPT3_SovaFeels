1aa04068d78d69da81d5f7c39d64678e1a22f6ea
jesse deij <jessedeij@hotmail.com>
2017-01-19 11:42:46 +0100

    shop reword
    geen dropdown maar fanceh platjus en optimalisatie

---
+++ CustomPlatformerController/Assets/ButtonItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonItem : MonoBehaviour {
+
+    private static ShopScreenManager man;
+    public int index;
+
+    void Start()
+    {
+        if (man == null)
+        {
+            man = GameObject.Find("shopScreenManager").GetComponent<ShopScreenManager>();
+        }
+    }
+
+    public void Trigger()
+    {
+        man.selectItem(index);
+        Debug.Log(index);
+    }
+}
